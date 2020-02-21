4cf983b0fe2bfea18367f809c342b273c04e0d5b
Travis Krause <tkrause@adobe.com>
2020-02-20 22:49:54 -0600

    media stuff

--- equidrop/settings.py
+++ equidrop/settings.py
@@ -15,8 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -121,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = 'media'
+MEDIA_URL = os.path.join(BASE_DIR, 'media')
