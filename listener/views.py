0f918178be9fe288cb139006a51c92d5ba3815c4
Travis Krause <tkrause@adobe.com>
2020-08-03 15:23:25 -0500

    disable csrf

--- listener/views.py
+++ listener/views.py
@@ -1,9 +1,27 @@
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import ListenerLog
 
+
+@csrf_exempt
 def listener(request):
+    """
+    {
+        "name": "Toolbox",
+        "scope": "ACCOUNT",
+        "state": "ACTIVE",
+        "webhookSubscriptionEvents": [
+            "AGREEMENT_ALL"
+        ],
+        "webhookUrlInfo": {
+            "url": "https://adobe.krause.im/listener"
+        },
+        "applicationDisplayName": "Toolbox"
+    }
+    """
     client_id = request.headers.get("HTTP_X_ADOBESIGN_CLIENTID")
     data = request.body.decode("utf-8")
     log = ListenerLog.objects.create(data=data)
-    return JsonResponse({"xAdobeSignClientId": client_id})
+    response = JsonResponse({"xAdobeSignClientId": client_id})
+    return response
